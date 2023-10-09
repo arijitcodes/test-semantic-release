683ab34521c95cad03667ddc45d28035ab73af4d
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2023-10-10 01:23:54 +0530

    feat(index): re-Wrote the whole base
    Re-wrote the whole main function for the test
    
    BREAKING CHANGE: The function wont work with args anymore. You have to call it and pass the name.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-const args: string[] = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log("\nHello World! 🙋🏻‍♂️\n");
-} else {
-  const [name, userName]: string[] = args[0].split("=");
-  console.log(`\nHey there ${userName}! 👋🏻\n`);
-}
+export const sayHi = (name: string | null = null): void => {
+  if (!name) {
+    console.log("Hello World!");
+  } else {
+    console.log(`Hello ${name}! 🙋🏻‍♂️`);
+  }
+};
