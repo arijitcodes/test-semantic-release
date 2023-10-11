c9e41f53d870b3b9bee3e8cb0899738738cc441d
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2023-10-12 00:41:50 +0530

    fix(index): updated Log
    Updated the log output

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export const sayHi = (name: string | null = null): void => {
   if (!name) {
     console.log("Hello World!");
   } else {
-    console.log(`Hello ${name}! 🙋🏻‍♂️`);
+    console.log(`Hello ${name}!! 🙋🏻‍♂️`);
   }
 };
